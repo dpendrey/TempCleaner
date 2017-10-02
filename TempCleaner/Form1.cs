b74a823b1b1775ce7203b3032d3d541727f60507
dpendrey <david@pendrey.net.au>
2017-10-02 11:53:07 +1000

    Update
    * Now provides feedback at the end of the job

--- TempCleaner/Form1.cs
+++ TempCleaner/Form1.cs
@@ -12,23 +12,36 @@ namespace TempCleaner
 {
     public partial class Form1 : Form
     {
+        int filesDeleted = 0,
+            directoriesDeleted = 0;
+        long bytesDeleted = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            System.Threading.Thread thr = new System.Threading.Thread(new System.Threading.ThreadStart(runClean));
+            thr.Start();
         }
 
         private void runClean()
         {
+            filesDeleted = 0;
+            directoriesDeleted = 0;
+            bytesDeleted = 0;
+
             deleteDirectoryContents(@"C:\Windows\Temp\");
 
             string[] users = System.IO.Directory.GetDirectories(@"C:\Users\");
-            foreach(string userPath in users)
+            foreach (string userPath in users)
             {
                 if (System.IO.Directory.Exists(System.IO.Path.Combine(userPath, @"AppData\Local\Temp")))
                 {
                     deleteDirectoryContents(System.IO.Path.Combine(userPath, @"AppData\Local\Temp"));
                 }
             }
+
+            MessageBox.Show("Removed " + filesDeleted.ToString() + " files from " + directoriesDeleted.ToString() + " directories, saving a total of " + bytesToString(bytesDeleted));
         }
 
         private bool deleteDirectoryContents(string Path)
@@ -38,15 +51,17 @@ namespace TempCleaner
 
             #region Delete each sub-directory
             paths = System.IO.Directory.GetDirectories(Path);
-            foreach(string path in paths)
+            foreach (string path in paths)
             {
                 if (deleteDirectoryContents(path))
                 {
                     try
                     {
                         System.IO.Directory.Delete(path);
+                        directoriesDeleted++;
                     }
-                    catch (Exception) {
+                    catch (Exception)
+                    {
                         retVal = false;
                     }
                 }
@@ -57,11 +72,14 @@ namespace TempCleaner
 
             #region Delete each file
             paths = System.IO.Directory.GetFiles(Path);
-            foreach(string path in paths)
+            foreach (string path in paths)
             {
                 try
                 {
+                    System.IO.FileInfo info = new System.IO.FileInfo(path);
                     System.IO.File.Delete(path);
+                    filesDeleted++;
+                    bytesDeleted += info.Length;
                 }
                 catch (Exception)
                 {
@@ -71,6 +89,26 @@ namespace TempCleaner
             #endregion
 
             return retVal;
+        }
+
+        private string bytesToString(long Bytes)
+        {
+            if (Bytes > 1.5 * 1024)
+            {
+                if (Bytes > 1.5 * 1024 * 1024)
+                {
+                    if (Bytes > 1.5 * 1024 * 1024 * 1024)
+                    {
+                        return (Bytes / 1024.0 / 1024.0 / 1024.0).ToString("0.00") + "  Gb";
+                    }
+                    else
+                        return (Bytes / 1024.0 / 1024.0).ToString("0.00") + "  Mb";
+                }
+                else
+                    return (Bytes / 1024.0).ToString("0.00") + "  kb";
+            }
+            else
+                return Bytes.ToString() + "  b";
         }
     }
 }
